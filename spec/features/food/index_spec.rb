b7871bb6a761a4b46b56b7522032e70267c9aa41
Isaac Alter <i_alter@yahoo.com>
2023-04-08 18:49:59 -0600

    Add refactor: complete test logic to ensure code is working correct and in line with user story

--- spec/features/food/index_spec.rb
+++ spec/features/food/index_spec.rb
@@ -26,11 +26,10 @@ RSpec.describe "Food Index Page" do
     end
 
     expect(current_path).to eq(foods_path)
-    expect(page).to have_content("Results")
-    # expect(page).to have_content(10)
+    expect(page).to have_content("Total hits: 51270")
   end
 
-  it "I fill in the search form with 'sweet potatoes' and click 'search' I should see a total of the number of results" do 
+  it "I fill in the search form with 'sweet potatoes' and click 'search' I should see a list of TEN foods related to sweet potatoes" do 
       
     within(".search-field") do
       fill_in(:q, with: "sweet potatoes")
@@ -38,7 +37,12 @@ RSpec.describe "Food Index Page" do
     end
 
     expect(current_path).to eq(foods_path)
-    expect(page).to have_content("Results")
-    # expect(page).to have_content(10)
+
+    within(".food_list") do
+      expect(page).to have_selector("li", count: 10)
+      expect(page).to have_content("8901020020844, SWEET POTATOES, NOT A BRANDED ITEM, ORGANIC SWEET POTATOES.")
+      expect(page).to have_content("826561500014, SWEET POTATOES, Spring Acres Sales Company Inc.,")
+      expect(page).to have_content("638882125773, SWEET POTATOES, Stahlbush Island Farms, Inc., SWEET POTATOES.")
+    end   
   end
 end
